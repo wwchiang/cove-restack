ed3b06df2f01e3319bc97e6300adfcc7a4d5f5b2
Roderick Lisam <rlisam@gmail.com>
2014-11-12 19:41:02 -0500

    Hooking up date picker

--- source/js/colony/options-colony.js
+++ source/js/colony/options-colony.js
@@ -1,4 +1,9 @@
 
+colonyOptionsModule.controller('dateController', ['$scope', function(scope) {
+    scope.dobStart = "";
+    scope.dobEnd = "";
+}]);
+
 colonyOptionsModule.directive('colonyoptions', function() {
     return {
         restrict: 'E',
@@ -63,3 +68,42 @@ colonyOptionsModule.directive('genderFilter', [function() {
         }
     };
 }]);
+
+colonyOptionsModule.directive('datePicker', function() {
+    return {
+      // Enforce the angularJS default of restricting the directive to
+      // attributes only
+      restrict: 'A',
+      // Always use along with an ng-model
+      require: '?ngModel',
+      scope: {
+        // This method needs to be defined and
+        // passed in to the directive from the view controller
+        //select: '&'        // Bind the select function we refer to the
+                           // right scope
+      },
+      link: function(scope, element, attrs, ngModel) {
+        if (!ngModel) return;
+
+        var optionsObj = {};
+
+        optionsObj.dateFormat = 'mm/dd/yy';
+        var updateModel = function(dateTxt) {
+          scope.$apply(function () {
+            // Call the internal AngularJS helper to
+            // update the two-way binding
+            ngModel.$setViewValue(dateTxt);
+          });
+        };
+
+        optionsObj.onSelect = function(dateTxt, picker) {
+          updateModel(dateTxt);
+        };
+
+        ngModel.$render = function() {
+          // Use the AngularJS internal 'binding-specific' variable
+          element.datepicker('setDate', ngModel.$viewValue || '');
+        };
+      }
+    };
+  });
